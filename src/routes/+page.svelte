834de572241a487cbd7eafa0cdbfa4a16f8e2541
Youssef Lmouden <youssef.pro.coding@gmail.com>
2026-03-12 22:01:39 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let name = "Yousafe";
+</script>
+<div class="m-32">
+<h1 class="text-7xl mb-5">
+  Hai! my names {name}!
+</h1>
+<p class="text-3xl">I'm a 15yo (soon 16yo) dev from morrocco.</p>
+<p class="text-3xl">Im a <i class="nf nf-dev-rust"></i> rusty developper, and i code a lot of backend or nerdy stuff.</p> 
+<p class="text-3xl">and im also a <span class="text-[#FCF434]">non</span>bin<span  class="text-[#9C59D1]">ary</span> <span class="text-[#A3A3A3]">ase</span>xu<span class="text-[#800080]">al</span> furry <i class="nf nf-md-cat"></i>.</p>
+  <p class="text-3xl">I play undertale/deltarune a LOT.</p>
+  <p class="text-3xl">this is my very first website OwO.</p>
+<a class="nes-btn is-dark" href="#"> <i class="nes-icon github is-medium"></i>
+Normal</a>
+</div>
+
+
+
