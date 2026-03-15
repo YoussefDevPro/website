83b0ba4a935c0326f9e5d9c514e7036d6e966ab5
Youssef Lmouden <youssef.pro.coding@gmail.com>
2026-03-15 16:57:47 +0000

    aaa

--- svelte.config.js
+++ svelte.config.js
@@ -8,8 +8,15 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-      fallback: 'soontm.html'
-    })
+      fallback: '404.html',
+            pages: 'build',
+            assets: 'build',
+            precompress: false,
+            strict: true
+    }),
+    paths: {
+            base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+        }
 	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) => ({ runes: !filename.includes('node_modules') })
